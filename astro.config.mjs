d041dcfb424fb5f943cd48d417036dc793fb8582
fofariak <32439453+fofariak@users.noreply.github.com>
2025-09-10 16:22:45 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"; // This line is required
 
+// https://astro.build/config
 export default defineConfig({
+  // This line activates the Tailwind integration
   integrations: [tailwind()]
 });
