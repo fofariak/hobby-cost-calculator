560ae20b5da06a8ba060072affa9f1cb3a64e3dc
fofariak <32439453+fofariak@users.noreply.github.com>
2025-09-15 15:53:09 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind"; // This line is required
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify"; // This will be added
 
 // https://astro.build/config
 export default defineConfig({
-  // This line activates the Tailwind integration
-  integrations: [tailwind()]
+  // This property is added to enable server-side rendering
+  output: 'server',
+  
+  // Your integrations array is updated to include netlify()
+  integrations: [tailwind(), netlify()]
 });
